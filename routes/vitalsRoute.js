a8fb2bde637b77d57194af2c8a0de7cea9de1c1b
Maryam Abdelraheeem <maryamabldelraheem86@gmail.com>
2025-07-08 12:05:35 +0300

    edit the vitals

--- routes/vitalsRoute.js
+++ routes/vitalsRoute.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { getAllVitals } = require('../controllers/vitalsController');
+
+
+/**
+ * @route /api/vitals
+ * @access protected
+ */
 router
     .route('/')
     .get(getAllVitals)
